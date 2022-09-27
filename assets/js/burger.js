3601119e9df22c09309e2d8fec51b0df808a6e59
trbrtb <76260832+trbrtb@users.noreply.github.com>
2022-09-27 21:33:47 +0400

    1

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -3,7 +3,6 @@ $(function () {
 	$("#burger").on("click", function (event) {
 		event.preventDefault();
 
-//		$(this).toggleClass("active");
 		$("#burger").toggleClass("active");
 		$("#body").toggleClass("active");
 		$("#header").toggleClass("active");
@@ -14,17 +13,14 @@ $(function () {
 	});
 });
 
-$(function () {
+$('#nav').on('click', function () {
 
-	$("#nav").on("click", function (event) {
-		event.preventDefault();
-
-		$("#burger").removeClass("active");
-		$("#body").removeClass("active");
-		$("#header").removeClass("active");
-		$("#logo_black").removeClass("active");
-		$("#logo_white").removeClass("active");
-		$("#rct").removeClass("active");
-		$("#nav").removeClass("active");
-	});
+	$("#burger").removeClass("active");
+	$("#body").removeClass("active");
+	$("#header").removeClass("active");
+	$("#logo_black").removeClass("active");
+	$("#logo_white").removeClass("active");
+	$("#rct").removeClass("active");
+	$("#nav").removeClass("active");
+	//	$('#burger').hide();
 });
