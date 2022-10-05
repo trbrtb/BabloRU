55882743a722da2b90dd080145e990b6ca7f9057
trbrtb <76260832+trbrtb@users.noreply.github.com>
2022-10-05 22:44:48 +0400

    1

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,26 +1,13 @@
-$(function () {
-
-	$("#burger").on("click", function (event) {
-		event.preventDefault();
-
-		$("#burger").toggleClass("active");
-		$("#body").toggleClass("active");
-		$("#header").toggleClass("active");
-		$("#logo_black").toggleClass("active");
-		$("#logo_white").toggleClass("active");
-		$("#rct").toggleClass("active");
-		$("#nav").toggleClass("active");
+$(document).ready(function () {
+	$('.header_burger').click(function (event) {
+		$('.header_burger,.header_menu,.logo').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
 });
 
-$('#nav').on('click', function () {
-
-	$("#burger").removeClass("active");
-	$("#body").removeClass("active");
-	$("#header").removeClass("active");
-	$("#logo_black").removeClass("active");
-	$("#logo_white").removeClass("active");
-	$("#rct").removeClass("active");
-	$("#nav").removeClass("active");
-	//	$('#burger').hide();
+$(document).ready(function () {
+	$('.nav_link').click(function (event) {
+		$('.header_burger,.header_menu,.logo').removeClass('active');
+		$('body').removeClass('lock');
+	});
 });
