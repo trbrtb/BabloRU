afa6c06be033541ce85d2cefde47968e34b5a2f2
trbrtb <76260832+trbrtb@users.noreply.github.com>
2022-10-08 17:16:01 +0400

    1

--- assets/js/modal_feedback.js
+++ assets/js/modal_feedback.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-	document.querySelector('[data-target="modal"]').addEventListener('click', function (e) {
-		e.preventDefault();
-		document.body.style.overflow = 'hidden';
-		document.querySelector('#modal').style.marginLeft = scrollbar;
-		document.querySelector('#modal').classList.add('modal-open');
+$(document).ready(function () {
+	$('.social_margin').click(function (event) {
+		$('.modal,.modal_wrapper,.modal_header,.close_modal').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
-	document.querySelector('[data-target="close"]').addEventListener('click', function (e) {
-		e.preventDefault();
-		document.body.style.overflow = 'visible';
-		document.querySelector('#modal').style.marginLeft = '0px';
-		document.querySelector('#modal').classList.remove('modal-open');
+});
+
+$(document).ready(function () {
+	$('.close_modal').click(function (event) {
+		$('.modal').removeClass('active');
+		$('body').removeClass('lock');
 	});
 });
