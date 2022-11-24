1bdac7b9c86eb96a3bf7676f1aa66110c1faf41e
trbrtb <76260832+trbrtb@users.noreply.github.com>
2022-11-24 15:11:37 +0400

    1

--- assets/js/modal_feedback.js
+++ assets/js/modal_feedback.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
-	$('.modal_feedback').click(function (event) {
+	$('.open_modal_feedback').click(function (event) {
 		$('.modal_feedback').toggleClass('active');
-		$('body').toggleClass('lock');
 	});
 });
 
 $(document).ready(function () {
 	$('.close_modal_feedback').click(function (event) {
 		$('.modal_feedback').removeClass('active');
-		$('body').removeClass('lock');
 	});
 });
