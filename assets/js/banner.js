5ba7e5261919677308f2454c25e1878a442aa19e
trbrtb <trbrtb@gmail.com>
2023-01-14 01:04:46 +0400

    1

---
+++ assets/js/banner.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.close_banner').click(function (event) {
+        $('.banner').addClass('active');
+    });
+});
--- assets/js/fullpage_option.js
+++ assets/js/fullpage_option.js
@@ -1,8 +1,8 @@
 var myFullpage = new fullpage('#fullpage', {
 	//	sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
-	anchors: ['intro', 'services', 'work', 'contacts', 'about', 'reviews'],
+	anchors: ['intro', 'services', 'work', 'about', 'contacts', 'reviews'],
 	navigation: false,
-	navigationTooltips: ['intro', 'services', 'work', 'contacts', 'about', 'reviews'],
+	navigationTooltips: ['intro', 'services', 'contacts', 'work', 'about', 'reviews'],
 	showActiveTooltip: false,
 	menu: '#menu'
 });
