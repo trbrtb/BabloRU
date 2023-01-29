5a15400d5279b252c28c5bb4cddc8cb11e9cf52d
trbrtb <trbrtb@gmail.com>
2023-01-29 15:39:18 +0400

    1

--- assets/js/fullpage_option.js
+++ assets/js/fullpage_option.js
@@ -1,8 +1,8 @@
 var myFullpage = new fullpage('#fullpage', {
 	//	sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
-	anchors: ['intro', 'services', 'work', 'about', 'contacts'],
+	anchors: ['intro', 'services', 'work', 'about', 'contacts', 'reviews'],
 	navigation: false,
-	navigationTooltips: ['intro', 'services', 'work', 'about', 'contacts'],
+	navigationTooltips: ['intro', 'services', 'work', 'about', 'contacts', 'reviews'],
 	showActiveTooltip: false,
 	menu: '#menu'
 });
