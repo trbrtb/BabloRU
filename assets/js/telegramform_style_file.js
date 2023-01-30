828f0e2fae93e4d06bd0a48f9e807e449fc27dae
trbrtb <trbrtb@gmail.com>
2023-01-31 00:49:44 +0400

    1

--- assets/js/telegramform_style_file.js
+++ assets/js/telegramform_style_file.js
@@ -1,17 +1,17 @@
-$('.contact_form_file_button input[type=file]').on('change', function () {
+$('.telegram_form_file input[type=file]').on('change', function () {
     let filesNumber = this.files.length;
 
     if (filesNumber) {
-        $(this).closest('.contact_form_file_button').find('.contact_form_file_text_1').html('Выбрано файлов: ' + filesNumber);
-        $(".btn_del").removeClass("hide");
-        $(".contact_form_file_text_1").removeClass("hide");
-        $(".contact_form_file_text_2").addClass("hide");
+        $(this).closest('.telegram_form_file').find('.telegram_file_text_1').html('Выбрано файлов: ' + filesNumber);
+        $(".btn_del_telegram").removeClass("hide");
+        $(".telegram_file_text_1").removeClass("hide");
+        $(".telegram_file_text_2").addClass("hide");
     }
 
-    $('.btn_del').click(function (event) {
-        $(".btn_del").addClass("hide");
+    $('.btn_del_telegram').click(function (event) {
+        $(".btn_del_telegram").addClass("hide");
         $("#contact_form_input_file").val("");
-        $(".contact_form_file_text_1").addClass("hide");
-        $(".contact_form_file_text_2").removeClass("hide");
+        $(".telegram_file_text_1").addClass("hide");
+        $(".telegram_file_text_2").removeClass("hide");
     });
 });
