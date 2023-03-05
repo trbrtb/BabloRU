e7295e3b5dea3df0bc30f9cc8be2b0ac91c49a6e
trbrtb <trbrtb@gmail.com>
2023-03-06 01:40:56 +0400

    1

--- assets/js/choice_file_style.js
+++ assets/js/choice_file_style.js
@@ -12,3 +12,18 @@ $('.contact_choice_file_button input[type=file]').on('change', function () {
         $("#contact_input_file").val("");
     });
 });
+
+$('.reviews_choice_file_button input[type=file]').on('change', function () {
+    let filesNumber = this.files.length;
+
+    if (filesNumber) {
+        $(this).closest('.reviews_choice_file_button').find('.reviews_choice_file_text').html('Выбрано файлов: ' + filesNumber);
+        $(".reviews_choice_file_del").removeClass("hide");
+    }
+
+    $('.reviews_choice_file_del').click(function (event) {
+        $('.reviews_choice_file_text').html('Прикрепить файл');
+        $(".reviews_choice_file_del").addClass("hide");
+        $("#file-1").val("");
+    });
+});
